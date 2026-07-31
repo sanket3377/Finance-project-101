85fb75bbaa8dd5f3a0de7d0d9587e32d7a918054
sanket3377 <sangitagupta3737@gmail.com>
2026-07-31 13:11:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # For development. Later we'll restrict this.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DataRequest(BaseModel):
     company: str
     data_items: List[str]
     timelines: List[str]
 
-
 @app.get("/")
 async def home():
-    return {
-        "message": "Backend is running successfully!"
-    }
-
+    return {"message": "Backend is running successfully!"}
 
 @app.post("/fetch")
 async def fetch_data(request: DataRequest):
