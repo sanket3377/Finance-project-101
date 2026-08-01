53925e3a4bd2c41ad89129133a6b03eb91e41c7c
sanket3377 <sangitagupta3737@gmail.com>
2026-08-01 11:11:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from scraper.search import search_company
 from scraper.search import get_company_soup
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For testing. Later, replace "*" with your frontend URL.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class UserRequest(BaseModel):
